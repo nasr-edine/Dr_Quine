df39d695d88895bebb9f05464accb7fd9bae1934
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2019-03-07 15:33:22 +0100

    remove file Grace.c, Makefile and Sully.c

--- C/Grace.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#define A 1
-#define B 2
-#define QUINE(x)int main(){ char*s=#x;x}
-/*
-   comment
-*/
-QUINE(FILE * fp;fp = fopen ("Grace_kid.c", "w+");fprintf(fp, "#include <stdio.h>\n#define A 1\n#define B 2\n#define QUINE(x)int main(){ char*s=#x;x}\n/*\n   comment\n*/\nQUINE(%s)",s);fclose(fp);)
