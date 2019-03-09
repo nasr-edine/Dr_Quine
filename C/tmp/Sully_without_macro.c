2ee9819c8f2fedd7a2e06bb4ff5d0d476f2b45aa
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2019-03-09 10:00:06 +0100

    add fprintf function for print the source on the new file

--- C/tmp/Sully_without_macro.c
+++ C/tmp/Sully_without_macro.c
@@ -1,2 +1,2 @@
 #include<stdio.h>
-main(){int i=5;char name[10] = "Sully_X.c";FILE * fp;fp = fopen (name, "w+");char*s="#include<stdio.h>%cmain(){int i=%i;char name[10] = %cSully_X.c%c;FILE * fp;fp = fopen (name, %cw+%c);char*s=%c%s%c;fclose(fp);printf(s,10,--i,34,34,34,34,34,s,34,10);}%c";fclose(fp);printf(s,10,--i,34,34,34,34,34,s,34,10);}
+main(){int i=5;char name[10] = "Sully_X.c";FILE * fp;fp = fopen (name, "w+");char*s="#include<stdio.h>%cmain(){int i=%i;char name[10] = %cSully_X.c%c;FILE * fp;fp = fopen (name, %cw+%c);char*s=%c%s%c;fprintf(fp, s,10,--i,34,34,34,34,34,s,34,10);fclose(fp);printf(s,10,--i,34,34,34,34,34,s,34,10);}%c";fprintf(fp, s,10,--i,34,34,34,34,34,s,34,10);fclose(fp);printf(s,10,--i,34,34,34,34,34,s,34,10);}
